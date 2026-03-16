4732eca3a6bcc899962fa4c6efe98b1c0dedb8e8
alejo-3612 <alejandro.sanchez11@eia.edu.co>
2026-03-16 12:38:03 -0500

    Primer commit limpio

---
+++ SalaFinder/Models/AuditLog.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SalaFinder.Models
+{
+    public class AuditLog
+    {
+        [Key] 
+        public Guid id_log { get; set; } = Guid.NewGuid();
+        [Required]
+        public string userId { get; set; }
+
+        [ForeignKey("userId")]
+        public IdentityUser User { get; set; }
+        
+        public string action { get; set; }
+
+        public string entity { get; set; }
+
+        public DateTime timestamp { get; set; } = DateTime.UtcNow;
+    }
+}
